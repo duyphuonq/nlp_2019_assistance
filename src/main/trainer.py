36bbef214c50f810691810befeeb5e741781c548
kuribohkute <kuribohwithwing@gmail.com>
2019-11-18 22:42:50 +0700

    Try other models

--- src/main/trainer.py
+++ src/main/trainer.py
@@ -1,12 +1,19 @@
 
 from __future__ import division, print_function, unicode_literals
 import numpy as np
+import time
 from scipy.sparse import coo_matrix
-from sklearn.naive_bayes import MultinomialNB, BernoulliNB
 from sklearn.metrics import accuracy_score
 from src.constants import path_data, ROOT_DIR
 from src.utils.data_utils import get_dict_size
 from sklearn.model_selection import train_test_split
+
+from sklearn.naive_bayes import MultinomialNB, BernoulliNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import SGDClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 
 # data path and file name
 # import os
@@ -41,25 +48,96 @@ def read_data(data_fn, label_fn=None):
     return data
 
 
-def trained_model(x, y):
-    clf = BernoulliNB()
+models = {
+    "svm": SGDClassifier(tol = 1e-3, max_iter = 100),
+    "naive-bayes-0": BernoulliNB(),
+    "naive-bayes-1": MultinomialNB(),
+    "knn": KNeighborsClassifier(n_neighbors=1),
+    "svc": LinearSVC(),
+    "logistic-regression": LogisticRegression(random_state = 0, multi_class='auto', solver='lbfgs'),
+    "random-forest": RandomForestClassifier(n_estimators = 100, max_depth = 3, random_state = 0)
+}
+
+models_name = {
+    "svm": "SVM",
+    "naive-bayes-0": "Bernoulli NB",
+    "naive-bayes-1": "Multinomial NB",
+    "knn": "KNN",
+    "svc": "Linear SVC",
+    "logistic-regression": "Logistic Regression",
+    "random-forest": "Random Forest"
+}
+
+
+def trained_model(name, x, y):
+    clf = models[name]
     clf.fit(x, y)
-    print("train done")
+    # print("train done")
     return clf
 
 
-def test_accuracy():
+def test_accuracy(name):
     mean_result = 0
     limit = 100
+    average_time = 0
     for i in range(limit):
+        start_time = time.time()
+
         (train_data, train_label) = read_data(train_data_fn, train_label_fn)
         x, x_test, y, y_test = train_test_split(train_data, train_label, test_size=0.1, train_size=0.9)
-        clf = trained_model(x, y)
+        clf = trained_model(name, x, y)
         y_pred = clf.predict(x_test)
         local_result = accuracy_score(y_test, y_pred) * 100
-        print("Times: {} ({}%)".format(i + 1, local_result))
+        # print("Times: {} ({}%)".format(i + 1, local_result))
         mean_result += local_result
-    print('Average Accuracy = {:.2f}%'.format(mean_result / limit))
+
+        elapsed_time = time.time() - start_time
+        average_time += elapsed_time
+
+    average_time /= limit
+    print(
+        'Average results after {} iterations of {}:\nAverage Accuracy: {:.2f}%.\nAverage time elapsed: {:.2f}ms\n'
+            .format(
+            limit,
+            models_name[name],
+            mean_result / limit,
+            average_time
+        )
+    )
 
 
-test_accuracy()
+test_accuracy("naive-bayes-0")
+test_accuracy("naive-bayes-1")
+test_accuracy("svm")
+test_accuracy("knn")
+test_accuracy("svc")
+test_accuracy("logistic-regression")
+test_accuracy("random-forest")
+
+# Average results after 100 iterations of Bernoulli NB:
+# Average Accuracy: 90.74%.
+# Average time elapsed: 0.02ms
+#
+# Average results after 100 iterations of Multinomial NB:
+# Average Accuracy: 90.68%.
+# Average time elapsed: 0.02ms
+#
+# Average results after 100 iterations of SVM:
+# Average Accuracy: 94.56%.
+# Average time elapsed: 0.03ms
+#
+# Average results after 100 iterations of KNN:
+# Average Accuracy: 88.82%.
+# Average time elapsed: 0.03ms
+#
+# Average results after 100 iterations of Linear SVC:
+# Average Accuracy: 95.94%.
+# Average time elapsed: 0.04ms
+#
+# Average results after 100 iterations of Logistic Regression:
+# Average Accuracy: 97.36%.
+# Average time elapsed: 0.08ms
+#
+# Average results after 100 iterations of Random Forest:
+# Average Accuracy: 63.82%.
+# Average time elapsed: 0.36ms
